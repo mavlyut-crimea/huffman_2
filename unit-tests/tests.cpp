135b6d8818a0a9ae9ecf89a96783cb9190ea048e
mavlyut_crimea <90035059+mavlyut-crimea@users.noreply.github.com>
2022-08-23 22:53:52 +0300

    Update tests.cpp

--- unit-tests/tests.cpp
+++ unit-tests/tests.cpp
@@ -20,26 +20,27 @@ std::ostream& operator<<(std::ostream& out, MODES x) {
   return out << static_cast<int>(x);
 }
 
-#define ASSERT_EQ_FILES(in1, in2) {         \
-    std::ifstream fin1(in1), fin2(in2);     \
-    char tmp1, tmp2;                        \
-    while (!fin1.eof() && !fin2.eof()) {    \
-      fin1 >> tmp1;                         \
-      fin2 >> tmp2;                         \
-      ASSERT_EQ(tmp1, tmp2);                \
-    }                                       \
-    ASSERT_TRUE(fin1.eof() && fin2.eof());  \
+void ASSERT_EQ_FILES(char const* in1, char const* in2) {
+    std::ifstream fin1(in1), fin2(in2);
+    char tmp1 = 0, tmp2 = 0;
+    while (!fin1.eof() && !fin2.eof()) {
+      fin1 >> tmp1;
+      fin2 >> tmp2;
+      ASSERT_TRUE(tmp1 == tmp2);
+    }
+    ASSERT_TRUE(fin1.eof() && fin2.eof());
 }
 
 const std::string path = std::string(std::filesystem::current_path()) + "/unit-tests";
 
 template <typename T>
-void htest(char const* input, bool log = true) {
+void htest(std::string const& input, bool log = false) {
+  if (log) std::cout << path << "\n";
   std::string name_of_type(typeid(T).name());
-  std::cout << name_of_type << "_" << input << "_Test\n";
+  if (log) std::cout << name_of_type << "_" << input << "_Test\n";
   std::string in = path + "/files/" + input;
-  std::string enc = path + input + "_" + name_of_type + ".huf";
-  std::string dec = path + input + "_" + name_of_type + "_decomp";
+  std::string enc = path + "/enc_files/" + input + "_" + name_of_type;
+  std::string dec = path + "/dec_files/" + input + "_" + name_of_type;
   time_t t1 = std::time(nullptr);
   encode<T>(in.c_str(), enc.c_str());
   time_t t2 = std::time(nullptr);
@@ -51,13 +52,14 @@ void htest(char const* input, bool log = true) {
   ASSERT_EQ(s1, s3);
   double coef = static_cast<double>(s1) / static_cast<double>(s2);
   if (log) {
-    std::cout << "Start size: " << s1 << ", encoded_file size: " << s2
+    std::cout << "Start size: " << s1
+              << ", encoded_file size: " << s2
               << ", encode mode: " << get_mode_from_file(enc.c_str())
               << ", compression ratio: " << coef << "\n";
     std::cout << "Encode time: " << t2 - t1 << "s\n";
     std::cout << "Decode time: " << t3 - t2 << "s\n\n";
   }
-  ASSERT_EQ_FILES(in, dec)
+  ASSERT_EQ_FILES(in.c_str(), dec.c_str());
   if (s2 != 0) {
     ASSERT_TRUE(coef > 0.8 || s2 <= s1 + 416);
   }
@@ -78,25 +80,22 @@ void htest(char const* input, bool log = true) {
   }                                             \
   TEST(input, vector_long_long) {               \
     htest<ct_vector_ints<long long>>(#input);   \
+  }                                             \
+  TEST(input, vector_size_t) {                  \
+    htest<ct_vector_ints<size_t>>(#input);      \
   }
-
-// TEST(print, cwd) {
-//   std::cout << std::filesystem::current_path();
-//   for (const auto& file_name : std::filesystem::recursive_directory_iterator(std::filesystem::current_path())) {
-//     std::cout << file_name << "\n";
-//   }
-// }
 
 TEST(special, file_not_found) {
   ASSERT_THROW(encode("", ""), std::runtime_error);
 }
 
 TEST(special, empty_string) {
-  std::string enc = encode(std::string(""));
-  ASSERT_NE(enc, "");
+  std::cout << encode(std::string(""));
+  // I still don't agree, I want to implement EFO!
+  ASSERT_NE(encode(std::string("")), "");
 }
 
-TEST(special, check_string_return) {
+TEST(special, simple) {
   std::string str("abacaba");
   ASSERT_EQ(decode(encode(str)), str);
 }
@@ -105,9 +104,20 @@ TEST(special, unknown_mode) {
   ASSERT_ANY_THROW(decode("-120\nhrknf"));
 }
 
-TEST(special, broken_all_mode) {
-  ASSERT_ANY_THROW(decode("0"));
+TEST(special, no_mode) {
+  ASSERT_ANY_THROW(decode("hello world, i want error\n"));
 }
+
+TEST(special, no_mode_2) {
+  ASSERT_ANY_THROW(decode("текст на русском\n"));
+}
+
+TEST(special, broken_all_mode) {
+  ASSERT_ANY_THROW(decode("-1\n"));
+}
+
+// 0
+HTEST(empty)
 
 // 7 b
 HTEST(one_char)
@@ -152,4 +162,6 @@ HTEST(AAA)
 HTEST(full_abacaba)
 
 // 963 Mb, 216 Kb, 128 b
-HTEST(bigfile)
+TEST(bigfile, ct_default) {
+ htest<ct_default>("bigfile");
+}
