dacddc6baa7b4d96bea1113f3e87188e4b14b133
mavlyut_crimea <90035059+mavlyut-crimea@users.noreply.github.com>
2022-08-24 18:06:41 +0300

    Update tests.cpp

--- unit-tests/tests.cpp
+++ unit-tests/tests.cpp
@@ -36,35 +36,44 @@ void ASSERT_EQ_FILES(char const* in1, char const* in2) {
 const std::string path = std::string(std::filesystem::current_path()) + "/unit-tests";
 
 template <typename T>
-void htest(std::string const& input, bool log = false) {
-  if (log) std::cout << path << "\n";
+void htest(std::string const& input) {
+#ifdef LOG
+  std::cout << path << "\n";
+#endif
   std::string name_of_type(typeid(T).name());
-  if (log) std::cout << name_of_type << "_" << input << "_Test\n";
+#ifdef LOG
+  std::cout << name_of_type << "_" << input << "_Test\n";
+#endif
   std::string in = path + "/files/" + input;
-  std::string enc = path + input + "_" + name_of_type + ".huf";
-  std::string dec = path + input + "_" + name_of_type + "_decomp";
+  std::string enc = path + "/enc_files/" + input + "_" + name_of_type;
+  std::string dec = path + "/dec_files/" + input + "_" + name_of_type;
+#ifdef LOG
   time_t t1 = std::time(nullptr);
+#endif
   encode<T>(in.c_str(), enc.c_str());
+#ifdef LOG
   time_t t2 = std::time(nullptr);
+#endif
   decode<T>(enc.c_str(), dec.c_str());
+#ifdef LOG
   time_t t3 = std::time(nullptr);
+#endif
   size_t s1 = std::filesystem::file_size(in);
   size_t s2 = std::filesystem::file_size(enc);
   size_t s3 = std::filesystem::file_size(dec);
+  ASSERT_TRUE(s2 > 0);
   ASSERT_EQ(s1, s3);
   double coef = static_cast<double>(s1) / static_cast<double>(s2);
-  if (log) {
-    std::cout << "Start size: " << s1
-              << ", encoded_file size: " << s2
-              << ", encode mode: " << get_mode_from_file(enc.c_str())
-              << ", compression ratio: " << coef << "\n";
-    std::cout << "Encode time: " << t2 - t1 << "s\n";
-    std::cout << "Decode time: " << t3 - t2 << "s\n\n";
-  }
+#ifdef LOG
+  std::cout << "Start size: " << s1
+            << ", encoded_file size: " << s2
+            << ", encode mode: " << get_mode_from_file(enc.c_str())
+            << ", compression ratio: " << coef << "\n";
+  std::cout << "Encode time: " << t2 - t1 << "s\n";
+  std::cout << "Decode time: " << t3 - t2 << "s\n\n";
+#endif
   ASSERT_EQ_FILES(in.c_str(), dec.c_str());
-  if (s2 != 0) {
-    ASSERT_TRUE(coef > 0.8 || s2 <= s1 + 416);
-  }
+  ASSERT_TRUE(coef > 0.8 || s2 <= s1 + 416);
 }
 
 #define HTEST(input)                            \
@@ -92,8 +101,7 @@ TEST(special, file_not_found) {
 }
 
 TEST(special, empty_string) {
-  std::cout << encode(std::string(""));
-  // I still don't agree, I want to implement EFO!
+//   I still don't agree, I want to implement EFO!
   ASSERT_NE(encode(std::string("")), "");
 }
 
@@ -117,6 +125,9 @@ TEST(special, no_mode_2) {
 TEST(special, broken_all_mode) {
   ASSERT_ANY_THROW(decode("-1\n"));
 }
+
+// 0
+HTEST(empty)
 
 // 7 b
 HTEST(one_char)
@@ -145,6 +156,8 @@ HTEST(cpp_tutorial)
 // 356 Kb, 1008 b
 HTEST(some_program)
 
+#ifdef _ENABLE_BIG_TESTS
+
 // 1 Mb, 351 Kb, 718 b
 HTEST(organic_chemistry_en)
 
@@ -157,10 +170,37 @@ HTEST(organic_chemistry_in_4_volumes)
 // 16 Mb, 651 Mb, 365 b
 HTEST(AAA)
 
-// 63 Mb, 1023 Kb, 1023 b
-HTEST(full_abacaba)
+static constexpr size_t ALPHABET_SIZE = 26;
 
-// 963 Mb, 216 Kb, 128 b
-TEST(bigfile, ct_default) {
- htest<ct_default>("bigfile");
+static char get(size_t i) {
+  size_t ans = 0;
+  while (i % 2) {
+    i /= 2;
+    ans++;
+  }
+  return static_cast<char>(ans + 'a');
 }
+
+// 63 Mb, 1023 Kb, 1023 b
+TEST(full_abacaba, ct_default) {
+  srand(time(nullptr));
+  std::ofstream fout(path + "/files/full_abacaba", std::ios_base::out);
+  size_t end = (1L << ALPHABET_SIZE) - 1;
+  for (size_t i = 0; i < end; i++) {
+    fout << get(i);
+  }
+  htest<ct_default>("full_abacaba");
+}
+
+// 1 Gb
+TEST(bigfile, ct_default) {
+  srand(time(nullptr));
+  std::ofstream fout(path + "/files/bigfile", std::ios_base::out);
+  for (size_t i = 0; i < 1024 * 1024 * 1024; i++) {
+    fout << static_cast<char>(rand() % ALPHABET_SIZE + 'a');
+  }
+  fout.close();
+  htest<ct_default>("bigfile");
+}
+
+#endif
