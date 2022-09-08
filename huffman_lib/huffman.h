c989d1311c5e1c36fd88454ca0c17033d739f4b2
mavlyut <97634631+mavlyut@users.noreply.github.com>
2022-09-08 22:09:27 +0300

    huffman.h fixed
    openmodes |= std::ios_base::binary;
    using worker = ...;

--- huffman_lib/huffman.h
+++ huffman_lib/huffman.h
@@ -21,7 +21,7 @@ static void check_stream(std::basic_ios<char> const& stream) {
 template <typename T>
 struct hstream {
   static void start_work(T& x, char const* in) {
-    x.open(in, std::ios_base::in);
+    x.open(in, std::ios_base::binary | std::ios_base::in);
   }
 
   static void end_work(T& x) {
@@ -33,7 +33,7 @@ template <>
 struct hstream<std::stringstream> {
   static void start_work(std::stringstream& x, char const* in) {
     x.clear();
-    x = std::stringstream(in, std::ios_base::in);
+    x = std::stringstream(in, std::ios_base::binary | std::ios_base::in);
   }
 
   static void end_work(std::stringstream& x) {
@@ -47,16 +47,16 @@ template <typename code_t = huffman_code_type_examples::ct_default,
         && !std::is_same<huffman_code_type, code_t>::value, void*>::type = nullptr>
 void encode(char const* in, std::basic_ostream<char>& fout) {
   using stream_type = std::conditional_t<read_from_file, std::ifstream, std::stringstream>;
+  using worker = hstream<stream_type>;
   stream_type fin;
-  hstream<stream_type>::start_work(fin, in);
-  fin >> std::noskipws;
-  check_stream(fin);
+  worker::start_work(fin, in);
+  check_stream(fin >> std::noskipws);
   tree<code_t> tr;
   char tmp_char;
   while (fin >> tmp_char) {
     tr.inc(to_char_t(tmp_char));
   }
-  hstream<stream_type>::end_work(fin);
+  worker::end_work(fin);
   if (tr.get_cnt_used() == 1) {
     fout << MODES::ONE_CHAR << '\n';
     for (size_t i = 0; i < MAX_SIZE; i++) {
@@ -68,7 +68,7 @@ void encode(char const* in, std::basic_ostream<char>& fout) {
     return;
   }
   tr.build_tree();
-  hstream<stream_type>::start_work(fin, in);
+  worker::start_work(fin, in);
   fin >> std::noskipws;
   check_stream(fin);
   fout << tr;
@@ -77,14 +77,14 @@ void encode(char const* in, std::basic_ostream<char>& fout) {
     bout << tr.get_code(to_char_t(tmp_char));
   }
   bout.flush();
-  hstream<stream_type>::end_work(fin);
+  worker::end_work(fin);
 }
 
 template <typename code_t = huffman_code_type_examples::ct_default,
     typename std::enable_if<std::is_base_of<huffman_code_type, code_t>::value
         && !std::is_same<huffman_code_type, code_t>::value, void*>::type = nullptr>
 void encode(char const* in, char const* out) {
-  std::ofstream fout(out, std::ios_base::out);
+  std::ofstream fout(out, std::ios_base::binary | std::ios_base::out);
   check_stream(fout);
   encode<code_t, true>(in, fout);
   fout.close();
@@ -94,7 +94,7 @@ template <typename code_t = huffman_code_type_examples::ct_default,
     typename std::enable_if<std::is_base_of<huffman_code_type, code_t>::value
         && !std::is_same<huffman_code_type, code_t>::value, void*>::type = nullptr>
 std::string encode(std::string const& str) {
-  std::stringstream sout(std::ios_base::out);
+  std::stringstream sout(std::ios_base::binary | std::ios_base::out);
   encode<code_t, false>(str.c_str(), sout);
   return sout.str();
 }
@@ -135,7 +135,9 @@ void decode(std::basic_istream<char>& fin,
   ibstream bin(fin);
   char tmp;
   int imode = MODES::UNKNOWN;
-  bin >> imode;
+  if (!(fin >> std::noskipws >> imode >> tmp)) {
+    throw std::runtime_error("File was broken: no mode");
+  }
   if (imode == 0) {
     return;
   }
@@ -173,7 +175,7 @@ template <typename code_t = huffman_code_type_examples::ct_default,
     typename std::enable_if<std::is_base_of<huffman_code_type, code_t>::value
         && !std::is_same<huffman_code_type, code_t>::value, void*>::type = nullptr>
 void decode(char const* in, std::basic_ostream<char>& fout = std::cout) {
-  std::ifstream fin(in, std::ios_base::in);
+  std::ifstream fin(in, std::ios_base::binary | std::ios_base::in);
   check_stream(fin);
   decode<code_t>(fin, fout);
   fin.close();
@@ -183,7 +185,7 @@ template <typename code_t = huffman_code_type_examples::ct_default,
     typename std::enable_if<std::is_base_of<huffman_code_type, code_t>::value
         && !std::is_same<huffman_code_type, code_t>::value, void*>::type = nullptr>
 void decode(std::basic_istream<char>* fin, char const* out) {
-  std::ofstream fout(out, std::ios_base::out);
+  std::ofstream fout(out, std::ios_base::binary | std::ios_base::out);
   check_stream(fout);
   decode<code_t>(fin, fout);
   fout.close();
@@ -193,7 +195,7 @@ template <typename code_t = huffman_code_type_examples::ct_default,
     typename std::enable_if<std::is_base_of<huffman_code_type, code_t>::value
         && !std::is_same<huffman_code_type, code_t>::value, void*>::type = nullptr>
 void decode(char const* in, char const* out) {
-  std::ofstream fout(out, std::ios_base::out);
+  std::ofstream fout(out, std::ios_base::binary | std::ios_base::out);
   check_stream(fout);
   decode<code_t>(in, fout);
   fout.close();
@@ -203,8 +205,8 @@ template <typename code_t = huffman_code_type_examples::ct_default,
     typename std::enable_if<std::is_base_of<huffman_code_type, code_t>::value
         && !std::is_same<huffman_code_type, code_t>::value, void*>::type = nullptr>
 std::string decode(std::string const& str) {
-  std::stringstream sin(str, std::ios_base::in);
-  std::stringstream sout(std::ios_base::out);
+  std::stringstream sin(str, std::ios_base::binary | std::ios_base::in);
+  std::stringstream sout(std::ios_base::binary | std::ios_base::out);
   decode<code_t>(sin, sout);
   return sout.str();
 }
