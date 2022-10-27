ea114a1b5d32eab73b0d15d1aed7cbcedd9dcb46
mavlyut <97634631+mavlyut@users.noreply.github.com>
2022-10-27 23:34:03 +0300

    Update consts.h

--- huffman_lib/include/consts.h
+++ huffman_lib/include/consts.h
@@ -17,7 +17,7 @@ using weight_t = size_t;
 using ind_t = int16_t;
 constexpr size_t BYTESIZE = 8;
 constexpr size_t ALPHABET_SIZE = (1 << (sizeof(char_t) * BYTESIZE));
-constexpr size_t BUFSIZE = 8192;
+constexpr size_t BUFSIZE = 4096;
 constexpr size_t BYTEMASK = (1 << BYTESIZE) - 1;
 
 #endif // HUFFMAN_CONSTS_H
