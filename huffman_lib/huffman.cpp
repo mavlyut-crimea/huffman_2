ac525c76f81b64c284da1c5c014923933d5d98d6
mavlyut <97634631+mavlyut@users.noreply.github.com>
2022-10-27 23:23:42 +0300

    Update huffman.cpp

--- huffman_lib/huffman.cpp
+++ huffman_lib/huffman.cpp
@@ -18,13 +18,18 @@ void encode(char const* in, char const* out) {
   cntr.read_from_file(in);
   encode_huffman_tree tr(cntr);
   std::ifstream fin(in, std::ifstream::in);
-  check_stream(fin >> std::noskipws);
+  check_stream(fin);
   std::ofstream fout(out, std::ifstream::out);
   binary_writer bw(fout);
-  write(bw, tr, cntr.get_array());
-  char tmp = 0;
-  while (fin >> tmp)
-    bw.write(tr.get_code(tmp));
+  write(bw, tr);
+  auto rdbuf = fin.rdbuf();
+  std::array<char, BUFSIZE> buf{};
+  std::streamsize sz = -1;
+  while (sz != 0) {
+    sz = rdbuf->sgetn(buf.data(), BUFSIZE);
+    for (size_t i = 0; i < sz; i++)
+      bw.write(tr.get_code(buf[i]));
+  }
   bw.flush();
   fin.close();
 }
@@ -36,12 +41,20 @@ void decode(char const* in, char const* out) {
   binary_reader br(fin);
   decode_huffman_tree tr(br);
   ind_t tmp_node = 0;
+  std::string buf(BUFSIZE, 0);
+  size_t pos = 0;
   while (!br.eof()) {
     bool b = br.next_bool();
     tr.move(tmp_node, b);
     if (tr.is_leaf(tmp_node)) {
-      fout << tr.get_char(tmp_node);
+      buf[pos++] = tr.get_char(tmp_node);
+      if (pos == BUFSIZE) {
+        fout << buf;
+        pos = 0;
+      }
       tmp_node = 0;
     }
   }
+  for (size_t i = 0; i < pos; i++)
+    fout << buf[i];
 }
