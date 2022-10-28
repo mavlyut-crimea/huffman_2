161257d8bf06ae8d14f2b74962e178f6e7f9d2d4
mavlyut <97634631+mavlyut@users.noreply.github.com>
2022-10-28 18:05:06 +0300

    Update tests.cpp

--- unit-tests/tests.cpp
+++ unit-tests/tests.cpp
@@ -1,44 +1,28 @@
 #include "../huffman_lib/include/consts.h"
 #include "../huffman_lib/include/huffman.h"
 #include "gtest/gtest.h"
+#include <filesystem>
 #include <fstream>
 
 void ASSERT_EQ_FILES(char const* in1, char const* in2) {
   std::ifstream fin1(in1, std::ios_base::in), fin2(in2, std::ios_base::in);
   char tmp1 = 0, tmp2 = 0;
-  size_t i = 0;
   while (!fin1.eof() && !fin2.eof()) {
     fin1 >> tmp1;
     fin2 >> tmp2;
-    if (tmp1 != tmp2)
-      std::cout << "At position " << i << " found " << tmp1 << " != " << tmp2 << '\n';
-    i++;
     ASSERT_EQ(tmp1, tmp2);
   }
-  ASSERT_TRUE(fin1.eof());
-  ASSERT_TRUE(fin2.eof());
+  ASSERT_TRUE(fin1.eof() && fin2.eof());
   fin1.close();
   fin2.close();
 }
 
 const std::string path = "unit-tests";
 
-void cat_file(std::string const& name) {
-  std::ifstream fin(name, std::ifstream::in);
-  std::cout << name << '\n';
-  char tmp;
-  while (fin >> tmp) {
-    std::cout << static_cast<int>(tmp) << ' ';
-  }
-  std::cout << '\n';
-  fin.close();
-}
-
-template <bool change = true>
 void htest(std::string const& input) {
-  std::string in = (change ? (path + "/files/") : "") + input;
-  std::string enc = (change ? (path + "/") : "") + input + ".huf";
-  std::string dec = (change ? (path + "/") : "") + input + "_decomp";
+  std::string in = path + "/files/" + input;
+  std::string enc = path + "/" + input + ".huf";
+  std::string dec = path + "/" + input + "_decomp";
   encode(in.c_str(), enc.c_str());
   decode(enc.c_str(), dec.c_str());
   ASSERT_EQ_FILES(in.c_str(), dec.c_str());
@@ -51,6 +35,20 @@ void htest(std::string const& input) {
 TEST(special, file_not_found) {
   ASSERT_THROW(encode("", ""), std::runtime_error);
 }
+
+#define THROW_DECODE_TEST(input) TEST(special, input) {               \
+    ASSERT_THROW(decode((path + "/files/"+ #input).c_str(),           \
+                 (path + "/files/" + #input + ".decomp").c_str()),    \
+                 std::runtime_error);                                 \
+  }
+
+THROW_DECODE_TEST(simple)
+
+THROW_DECODE_TEST(bad_tree)
+
+THROW_DECODE_TEST(no_rem)
+
+THROW_DECODE_TEST(short_tree)
 
 // 0
 HTEST(empty)
@@ -91,8 +89,6 @@ HTEST(java_tutorial)
 // 5 Mb, 788 Kb, 947 b
 HTEST(organic_chemistry_in_4_volumes)
 
-#ifdef _ENABLE_BIG_TESTS
-
 // 16 Mb, 651 Mb, 365 b
 HTEST(AAA)
 
@@ -114,7 +110,6 @@ TEST(correctness, full_abacaba) {
     fout << get(i);
   }
   fout.close();
-  std::cout << "File generated\n";
   htest("full_abacaba");
   std::filesystem::remove(new_path);
 }
@@ -128,9 +123,6 @@ TEST(correctness, bigfile) {
     fout << static_cast<char>(rand() % ALPHABET_SIZE + 'a');
   }
   fout.close();
-  std::cout << "File generated\n";
   htest("bigfile");
   std::filesystem::remove(new_path);
 }
-
-#endif
