9eb72898a99fb1bc89a02c507eb5547815edfd6e
mavlyut <97634631+mavlyut@users.noreply.github.com>
2022-10-28 17:08:51 +0300

    Update counter.h

--- huffman_lib/include/counter.h
+++ huffman_lib/include/counter.h
@@ -10,15 +10,21 @@
 #include <string>
 
 struct counter {
+  friend struct encode_huffman_tree;
+
   counter();
   ~counter();
 
   void append(char);
   void read_from_file(char const*);
+  weight_t get_truth_cnt(ind_t) const;
   weight_t operator[](ind_t) const;
 
 private:
+  ind_t used;
   std::array<weight_t, ALPHABET_SIZE> cnts{};
+
+  void normalize();
 };
 
 #endif // HUFFMAN_COUNTER_H
